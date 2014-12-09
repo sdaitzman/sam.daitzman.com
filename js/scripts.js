128a6f19cd58312e24bfae78f3ba65b65538b1a5
Sam Daitzman <samuel@daitzman.com>
2014-12-08 21:08:52 -0500

    added fancy animations 'n stuff

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,12 @@
-$('.things-i-am').typer();
+$.typer.options = {
+	highlightSpeed    : 50,
+	typeSpeed         : 200,
+	clearDelay        : 500,
+	typeDelay         : 200,
+	clearOnHighlight  : true,
+	typerDataAttr     : 'data-typer-targets',
+	typerInterval     : 5000
+}
+$('[data-typer-targets]').typer();
+
+$('body').toggleClass('done');
