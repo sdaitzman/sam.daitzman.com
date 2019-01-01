22f309cebd17c784b627bec7e1f9c41b20c36b7b
Sam Daitzman <sam@daitzman.com>
2019-01-01 13:00:23 -0500

    limits typer script to homepage

--- scripts.js
+++ scripts.js
@@ -1,21 +1,23 @@
 $(document).ready(function() {
 
-	$.typer.options = {
-		highlightSpeed: 100,
-		typeSpeed: 200,
-		clearDelay: 500,
-		typeDelay: 200,
-		clearOnHighlight: false,
-		typerDataAttr: 'data-typer-targets',
-		typerInterval: 3000
+	if($('body').hasClass('typer-page')) {
+		$.typer.options = {
+			highlightSpeed: 100,
+			typeSpeed: 200,
+			clearDelay: 500,
+			typeDelay: 200,
+			clearOnHighlight: false,
+			typerDataAttr: 'data-typer-targets',
+			typerInterval: 3000
+		}
+
+		setTimeout(() => {
+			$('[data-typer-targets]').typer()
+		}, 3000)
 	}
 
-	setTimeout(function() {
-		$('[data-typer-targets]').typer()
-	}, 3000)
 
 	$('body').toggleClass('done')
-
 	document.addEventListener("touchstart", function(){}, true)
 
 })
