5810592bddb0dc98fc2f4bdba90864c0cb9de508
Sam Daitzman <sam@daitzman.com>
2020-01-01 10:53:41 -0500

    initial vuepress install

---
+++ .vuepress/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    title: 'Sam Daitzman',
+    plugins: [
+        [
+            '@vuepress/google-analytics',
+            {
+                'ga': 'UA-57620806-1' // UA-00000000-0
+            }
+        ]
+    ],
+    'markdown': {
+        anchor: {
+            permalink: false,
+            permalinkBefore: true,
+            permalinkSymbol: '#'
+        }
+    }
+}
