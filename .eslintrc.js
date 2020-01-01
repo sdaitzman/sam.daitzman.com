9c033606b8d60caaf6bfa0a33bffe731711cfeae
Sam Daitzman <sam@daitzman.com>
2020-01-01 17:52:35 -0500

    applies standardjs linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,5 +19,9 @@ module.exports = {
     'vue'
   ],
   rules: {
+    'standard/object-curly-even-spacing': [2, 'either'],
+    'standard/array-bracket-even-spacing': [2, 'either'],
+    'standard/computed-property-even-spacing': [2, 'even'],
+    'standard/no-callback-literal': [2, ['cb', 'callback']]
   }
 }
