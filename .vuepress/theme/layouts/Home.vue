77816c2990a9555a2023e2e052c07855a82b99f6
Sam Daitzman <sam@daitzman.com>
2020-01-02 11:35:43 -0500

    fixes Home.vue formatting

--- .vuepress/theme/layouts/Home.vue
+++ .vuepress/theme/layouts/Home.vue
@@ -1,19 +1,20 @@
 <template>
-    <div class="all-container">
-        <div class="body-container">
-            <div class="body-content">
-                <Content />
-            </div>
-        </div>
-        <Nav />
+  <div class="all-container">
+    <div class="body-container">
+      <div class="body-content">
+        <Content />
+      </div>
     </div>
+    <Nav />
+  </div>
 </template>
 
-<style lang="scss"> @import '../styles/base.scss'; </style>
+<style lang="scss">
+@import "../styles/base.scss";
+</style>
 
 <style scoped lang="scss">
-    @import '../styles/variables.scss';
-
+@import "../styles/variables.scss";
 </style>
 
 <script>
