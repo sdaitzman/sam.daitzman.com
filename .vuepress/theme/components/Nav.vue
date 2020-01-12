0d67b139481837ae91c00be2eeb5abc9a0d2b024
Sam Daitzman <sam@daitzman.com>
2020-01-12 12:11:49 -0500

    basic working nav layout

--- .vuepress/theme/components/Nav.vue
+++ .vuepress/theme/components/Nav.vue
@@ -38,8 +38,8 @@
 
 .nav {
   display: flex;
-  height: 200px;
-  margin: 7px 7px 0;
+  margin: 20px 20px 0;
+  flex-direction: row;
 }
 
 .site-title {
@@ -52,47 +52,57 @@
   text-align: center;
   background: $nice-red;
   color: white;
+  white-space: nowrap;
 }
 
 .nav-links {
   margin: 0;
   display: flex;
+  flex-direction: row;
   list-style-type: none;
-  padding-left: 0;
+  padding: 30px 0;
+  align-self: flex-end;
+  border: 2px solid $nicegray;
+  border-left: 0;
+  border-bottom: 0;
 
-  a {
+  li {
     display: inline-flex;
-    font-size: 24px;
-    margin-right: 20px;
-    padding: 15px 7px;
-    text-decoration: none;
-    text-shadow: 2px 0 white, -2px 0 white, 0 0.2px white;
-    &.router-link-exact-active { font-weight: 400; }
+    height: 30px;
 
-    &::after {
-      content: '';
-      border-bottom: 1px solid black;
-      display: box;
-      box-sizing: border-box;
-      width: 100%;
-      position: relative;
-      top: -4px;
-      z-index: -3;
+    a {
+      display: inline-flex;
+      font-size: 24px;
+      margin-right: 20px;
+      text-decoration: none;
+      text-shadow: 2px 0 white, -2px 0 white, 0 0.2px white;
+      &.router-link-exact-active { font-weight: 400; }
+
+      // &::after {
+      //   content: '';
+      //   border-bottom: 1px solid black;
+      //   display: box;
+      //   box-sizing: border-box;
+      //   width: 100%;
+      //   position: relative;
+      //   top: -4px;
+      //   z-index: -3;
+      // }
     }
   }
 }
 
-.nav-links li:not(:last-child)::after {
-  display: inline-block;
-  content: '+';
-  width: 20px;
-  height: 20px;
-  color: black;
-  position: relative;
-  right: 3px;
-  pointer-events: none;
-  user-select: none;
-}
+// .nav-links li:not(:last-child)::after {
+//   display: inline-block;
+//   content: '+';
+//   width: 20px;
+//   height: 20px;
+//   color: black;
+//   position: relative;
+//   right: 3px;
+//   pointer-events: none;
+//   user-select: none;
+// }
 
 // this is a comment!
 </style>
