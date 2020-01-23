71c0e2d90a7436c7a81662963bee648ea81f7ff8
Sam Daitzman <sam@daitzman.com>
2020-01-22 21:38:46 -0500

    sets up netlify cms

--- .vuepress/theme/layouts/Layout.vue
+++ .vuepress/theme/layouts/Layout.vue
@@ -7,6 +7,18 @@
         <Content />
       </div>
     </div>
+    <script src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
+    <script>
+      if (window.netlifyIdentity) {
+      window.netlifyIdentity.on("init", user => {
+      if (!user) {
+      window.netlifyIdentity.on("login", () => {
+      document.location.href = "/admin/";
+      });
+      }
+      });
+      }
+    </script>
   </div>
 </template>
 
